b1677d1e488516a612bc4a360bb770014f34af67
Radu Voicilas <radu.voicilas@users.noreply.github.com>
2018-01-05 13:22:27 +0200

    Use realpath on --outfile
    When daemonizing, the current directory is changed, which makes
    inotifwait to fail if --outfile is not provided as an absolute pathname.

--- t/sharness.sh
+++ t/sharness.sh
@@ -812,7 +812,7 @@ SHARNESS_TEST_FILE="$0"
 export SHARNESS_TEST_FILE
 
 # Prepare test area.
-SHARNESS_TRASH_DIRECTORY="trash directory.$(basename "$SHARNESS_TEST_FILE" ".$SHARNESS_TEST_EXTENSION")"
+SHARNESS_TRASH_DIRECTORY="trash.directory.$(basename "$SHARNESS_TEST_FILE" ".$SHARNESS_TEST_EXTENSION")"
 test -n "$root" && SHARNESS_TRASH_DIRECTORY="$root/$SHARNESS_TRASH_DIRECTORY"
 case "$SHARNESS_TRASH_DIRECTORY" in
 /*) ;; # absolute path is good
