43e699f3929a0e1d7a4bee8202c165d741900c9a
T.C. Ferguson <tcf909@gmail.com>
2020-06-18 11:01:07 -0700

    Resolves #72
    Makes unique id (cookie) available using "%c" as a formatting option.
    
    Original code by mrspuratic

--- libinotifytools/src/inotifytools.c
+++ libinotifytools/src/inotifytools.c
@@ -1655,6 +1655,8 @@ int inotifytools_get_num_watches() {
  * The following tokens will be replaced with the specified string:
  *  \li \c \%w - This will be replaced with the name of the Watched file on
  *               which an event occurred.
+ *  \li \c \%c - This will be replaced with the cookie of the Watched file on
+ *               which an event occurred.
  *  \li \c \%f - When an event occurs within a directory, this will be replaced
  *               with the name of the File which caused the event to occur.
  *               Otherwise, this will be replaced with an empty string.
@@ -1699,6 +1701,8 @@ int inotifytools_printf( struct inotify_event* event, char* fmt ) {
  * @section syntax Format string syntax
  * The following tokens will be replaced with the specified string:
  *  \li \c \%w - This will be replaced with the name of the Watched file on
+ *               which an event occurred.
+ *  \li \c \%c - This will be replaced with the cookie of the Watched file on
  *               which an event occurred.
  *  \li \c \%f - When an event occurs within a directory, this will be replaced
  *               with the name of the File which caused the event to occur.
@@ -1754,6 +1758,8 @@ int inotifytools_fprintf( FILE* file, struct inotify_event* event, char* fmt ) {
  * The following tokens will be replaced with the specified string:
  *  \li \c \%w - This will be replaced with the name of the Watched file on
  *               which an event occurred.
+ *  \li \c \%c - This will be replaced with the cookie of the Watched file on
+ *               which an event occurred.
  *  \li \c \%f - When an event occurs within a directory, this will be replaced
  *               with the name of the File which caused the event to occur.
  *               Otherwise, this will be replaced with an empty string.
@@ -1806,15 +1812,14 @@ int inotifytools_sprintf( char * out, struct inotify_event* event, char* fmt ) {
  * The following tokens will be replaced with the specified string:
  *  \li \c \%w - This will be replaced with the name of the Watched file on
  *               which an event occurred.
+ *  \li \c \%c - This will be replaced with cookie of the Watched file on
+ *               which an event occurred.
  *  \li \c \%f - When an event occurs within a directory, this will be replaced
  *               with the name of the File which caused the event to occur.
  *               Otherwise, this will be replaced with an empty string.
  *  \li \c \%e - Replaced with the Event(s) which occurred, comma-separated.
  *  \li \c \%Xe - Replaced with the Event(s) which occurred, separated by
  *                whichever character is in the place of `X'.
- *  \li \c \%c - This will be replaced with the unique identifier (cookie)
- *               of the Watched file on which an event occurred. This allows
- *               two events to work together potentially (moved_from->moved_to).
  *  \li \c \%T - Replaced by the current Time in the format specified by the
  *               string previously passed to inotifytools_set_printf_timefmt(),
  *               or replaced with an empty string if that function has never
