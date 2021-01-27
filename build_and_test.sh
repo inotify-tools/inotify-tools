4f4806b5a40544c738ae52807670e9f760440348
NiLuJe <ninuje@gmail.com>
2021-01-27 23:22:06 +0100

    Add static builds to the CI script

--- build_and_test.sh
+++ build_and_test.sh
@@ -4,7 +4,7 @@ set -e
 
 j=10
 
-printf "gcc build\n"
+printf "gcc shared build\n"
 if [ "$1" == "clean" ]; then
   git clean -fdx 2>&1
 fi
@@ -29,7 +29,29 @@ cd t
 make -j$j
 cd -
 
-printf "\nclang build\n"
+printf "\ngcc static build\n"
+if [ "$1" == "clean" ]; then
+  git clean -fdx 2>&1
+fi
+
+export CC=gcc
+./autogen.sh
+./configure --enable-static --disable-shared
+make -j$j
+
+if [ "$os" != "FreeBSD" ]; then
+  printf "\nunit test\n"
+  cd libinotifytools/src/
+  make -j$j test
+  ./test
+  cd -
+fi
+
+printf "\nintegration test\n"
+cd t
+make -j$j
+
+printf "\nclang shared build\n"
 if [ "$1" == "clean" ]; then
   git clean -fdx 2>&1
 fi
@@ -51,3 +73,24 @@ printf "\nintegration test\n"
 cd t
 make -j$j
 
+printf "\nclang static build\n"
+if [ "$1" == "clean" ]; then
+  git clean -fdx 2>&1
+fi
+
+export CC=clang
+./autogen.sh
+./configure --enable-static --disable-shared
+make -j$j
+
+if [ "$os" != "FreeBSD" ]; then
+  printf "\nunit test\n"
+  cd libinotifytools/src/
+  make -j$j test
+  ./test
+  cd -
+fi
+
+printf "\nintegration test\n"
+cd t
+make -j$j
