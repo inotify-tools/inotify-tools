5541000e4cea10339c8ac8a79e0ec7c56eaef226
Eric Curtin <ecurtin@redhat.com>
2021-09-22 10:20:46 +0100

    rh_build
    So it works in our build and the fedora one

--- rh_build.sh
+++ rh_build.sh
@@ -3,15 +3,15 @@
 set -e
 
 if [ -n "$1" ]; then
-  j=$1
+  j="$1"
 else
   ./autogen.sh
   ./configure --disable-dependency-tracking --disable-static --enable-doxygen
-  j=16
+  j="-j16"
 fi
 
 # https://docs.fedoraproject.org/en-US/packaging-guidelines/#_removing_rpath
 sed -i 's|^hardcode_libdir_flag_spec=.*|hardcode_libdir_flag_spec=""|g' libtool
 sed -i 's|^runpath_var=LD_RUN_PATH|runpath_var=DIE_RPATH_DIE|g' libtool
-make -j$j
+make $j
 
