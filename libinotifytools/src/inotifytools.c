19370c62370b29ee44faa3d1c2de68a73e8a5089
Eric Curtin <ecurtin@redhat.com>
2022-01-03 12:46:35 +0000

    libinotifytools: Include limit.h for PATH_MAX

--- libinotifytools/src/inotifytools.c
+++ libinotifytools/src/inotifytools.c
@@ -17,21 +17,22 @@
 #include "inotifytools_p.h"
 #include "stats.h"
 
-#include <string.h>
-#include <strings.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <errno.h>
-#include <sys/select.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
 #include <dirent.h>
-#include <time.h>
+#include <errno.h>
+#include <limits.h>
 #include <regex.h>
 #include <setjmp.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "inotifytools/inotify.h"
 
