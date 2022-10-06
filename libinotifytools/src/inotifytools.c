inotify-tools/inotify-tools

commit dd59c5347af8dad232a1df4e922daa797c8f5ba1

Extract stats into their own file (#129)

libinotifytools/src/inotifytools.c
@@ -15,6 +15,7 @@
 #include "../../config.h"
 #include "inotifytools/inotifytools.h"
 #include "inotifytools_p.h"
+#include "stats.h"
 
 #include <string.h>
 #include <strings.h>
@@ -127,26 +128,13 @@
 #define INSTANCES_PATH    INOTIFY_PROCDIR "max_user_instances"
 
 static int inotify_fd;
-static unsigned  num_access;
-static unsigned  num_modify;
-static unsigned  num_attrib;
-static unsigned  num_close_nowrite;
-static unsigned  num_close_write;
-static unsigned  num_open;
-static unsigned  num_move_self;
-static unsigned  num_moved_to;
-static unsigned  num_moved_from;
-static unsigned  num_create;
-static unsigned  num_delete;
-static unsigned  num_delete_self;
-static unsigned  num_unmount;
-static unsigned  num_total;
-static int collect_stats = 0;
+
+int collect_stats = 0;
 
 struct rbtree *tree_wd = 0;
 struct rbtree *tree_filename = 0;
 static int error = 0;
-static int init = 0;
+int init = 0;
 static char* timefmt = 0;
 static regex_t* regex = 0;
 /* 0: --exclude[i], 1: --include[i] */
@@ -156,25 +144,7 @@ static int isdir( char const * path );
 void record_stats( struct inotify_event const * event );
 int onestr_to_event(char const * event);
 
-/**
- * @internal
- * Assert that a condition evaluates to true, and optionally output a message
- * if the assertion fails.
- *
- * @param  cond  Integer; if 0, assertion fails, otherwise assertion succeeds.
- *
- * @param  mesg  A human-readable error message shown if assertion fails.
- *
- * @section example Example
- * @code
- * int upper = 100, lower = 50;
- * int input = get_user_input();
- * niceassert( input <= upper && input >= lower,
- *             "input not in required range!");
- * @endcode
- */
-#define niceassert(cond,mesg) _niceassert((long)cond, __LINE__, __FILE__, \
-                                          #cond, mesg)
+
 
 #define nasprintf(...) niceassert( -1 != asprintf(__VA_ARGS__), "out of memory")
 
@@ -195,7 +165,7 @@ int onestr_to_event(char const * event);
  *
  * @param  mesg  A human-readable error message shown if assertion fails.
  */
-static void _niceassert( long cond, int line, char const * file,
+void _niceassert( long cond, int line, char const * file,
                   char const * condstr, char const * mesg ) {
 	if ( cond ) return;
 
@@ -345,29 +315,7 @@ void inotifytools_cleanup() {
 	rbdestroy(tree_filename); tree_filename = 0;
 }
 
-/**
- * @internal
- */
-void empty_stats(const void *nodep,
-                 const VISIT which,
-                 const int depth, void *arg) {
-    if (which != endorder && which != leaf) return;
-	watch *w = (watch*)nodep;
-	w->hit_access = 0;
-	w->hit_modify = 0;
-	w->hit_attrib = 0;
-	w->hit_close_nowrite = 0;
-	w->hit_close_write = 0;
-	w->hit_open = 0;
-	w->hit_move_self = 0;
-	w->hit_moved_from = 0;
-	w->hit_moved_to = 0;
-	w->hit_create = 0;
-	w->hit_delete = 0;
-	w->hit_delete_self = 0;
-	w->hit_unmount = 0;
-	w->hit_total = 0;
-}
+
 
 /**
  * @internal
@@ -407,45 +355,6 @@ void get_num(const void *nodep,
              const int depth, void *arg) {
     if (which != endorder && which != leaf) return;
 	++(*((int*)arg));
-}
-
-
-/**
- * Initialize or reset statistics.
- *
- * inotifytools_initialize() must be called before this function can
- * be used.
- *
- * When this function is called, all subsequent events will be tallied.
- * Statistics can then be obtained via the @a inotifytools_get_stat_* functions.
- *
- * After the first call, subsequent calls to this function will reset the
- * event tallies to 0.
- */
-void inotifytools_initialize_stats() {
-	niceassert( init, "inotifytools_initialize not called yet" );
-
-	// if already collecting stats, reset stats
-	if (collect_stats) {
-		rbwalk(tree_wd, empty_stats, 0);
-	}
-
-	num_access = 0;
-	num_modify = 0;
-	num_attrib = 0;
-	num_close_nowrite = 0;
-	num_close_write = 0;
-	num_open = 0;
-	num_move_self = 0;
-	num_moved_from = 0;
-	num_moved_to = 0;
-	num_create = 0;
-	num_delete = 0;
-	num_delete_self = 0;
-	num_unmount = 0;
-	num_total = 0;
-
-	collect_stats = 1;
 }
 
 /**
@@ -1402,202 +1311,6 @@ int inotifytools_watch_recursively_with_exclude( char const * path, int events,
 	int ret = inotifytools_watch_file( my_path, events );
 	if ( my_path != path ) free( my_path );
         return ret;
-}
-
-/**
- * @internal
- */
-void record_stats( struct inotify_event const * event ) {
-	if (!event) return;
-	watch *w = watch_from_wd(event->wd);
-	if (!w) return;
-	if ( IN_ACCESS & event->mask ) {
-		++w->hit_access;
-		++num_access;
-	}
-	if ( IN_MODIFY & event->mask ) {
-		++w->hit_modify;
-		++num_modify;
-	}
-	if ( IN_ATTRIB & event->mask ) {
-		++w->hit_attrib;
-		++num_attrib;
-	}
-	if ( IN_CLOSE_WRITE & event->mask ) {
-		++w->hit_close_write;
-		++num_close_write;
-	}
-	if ( IN_CLOSE_NOWRITE & event->mask ) {
-		++w->hit_close_nowrite;
-		++num_close_nowrite;
-	}
-	if ( IN_OPEN & event->mask ) {
-		++w->hit_open;
-		++num_open;
-	}
-	if ( IN_MOVED_FROM & event->mask ) {
-		++w->hit_moved_from;
-		++num_moved_from;
-	}
-	if ( IN_MOVED_TO & event->mask ) {
-		++w->hit_moved_to;
-		++num_moved_to;
-	}
-	if ( IN_CREATE & event->mask ) {
-		++w->hit_create;
-		++num_create;
-	}
-	if ( IN_DELETE & event->mask ) {
-		++w->hit_delete;
-		++num_delete;
-	}
-	if ( IN_DELETE_SELF & event->mask ) {
-		++w->hit_delete_self;
-		++num_delete_self;
-	}
-	if ( IN_UNMOUNT & event->mask ) {
-		++w->hit_unmount;
-		++num_unmount;
-	}
-	if ( IN_MOVE_SELF & event->mask ) {
-		++w->hit_move_self;
-		++num_move_self;
-	}
-
-	++w->hit_total;
-	++num_total;
-
-}
-
-unsigned int *stat_ptr(watch *w, int event)
-{
-	if ( IN_ACCESS == event )
-		return &w->hit_access;
-	if ( IN_MODIFY == event )
-		return &w->hit_modify;
-	if ( IN_ATTRIB == event )
-		return &w->hit_attrib;
-	if ( IN_CLOSE_WRITE == event )
-		return &w->hit_close_write;
-	if ( IN_CLOSE_NOWRITE == event )
-		return &w->hit_close_nowrite;
-	if ( IN_OPEN == event )
-		return &w->hit_open;
-	if ( IN_MOVED_FROM == event )
-		return &w->hit_moved_from;
-	if ( IN_MOVED_TO == event )
-		return &w->hit_moved_to;
-	if ( IN_CREATE == event )
-		return &w->hit_create;
-	if ( IN_DELETE == event )
-		return &w->hit_delete;
-	if ( IN_DELETE_SELF == event )
-		return &w->hit_delete_self;
-	if ( IN_UNMOUNT == event )
-		return &w->hit_unmount;
-	if ( IN_MOVE_SELF == event )
-		return &w->hit_move_self;
-	if ( 0 == event )
-		return &w->hit_total;
-	return 0;
-}
-
-/**
- * Get statistics by a particular watch descriptor.
- *
- * inotifytools_initialize_stats() must be called before this function can
- * be used.
- *
- * @param wd watch descriptor to get stats for.
- *
- * @param event a single inotify event to get statistics for, or 0 for event
- *              total.  See section \ref events.
- *
- * @return the number of times the event specified by @a event has occurred on
- *         the watch descriptor specified by @a wd since stats collection was
- *         enabled, or -1 if @a event or @a wd are invalid.
- */
-int inotifytools_get_stat_by_wd( int wd, int event ) {
-	if (!collect_stats) return -1;
-
-	watch *w = watch_from_wd(wd);
-	if (!w) return -1;
-	unsigned int *i = stat_ptr(w, event);
-	if (!i) return -1;
-	return *i;
-}
-
-/**
- * Get statistics aggregated across all watches.
- *
- * inotifytools_initialize_stats() must be called before this function can
- * be used.
- *
- * @param event a single inotify event to get statistics for, or 0 for event
- *              total.  See section \ref events.
- *
- * @return the number of times the event specified by @a event has occurred over
- *         all watches since stats collection was enabled, or -1 if @a event
- *         is not a valid event.
- */
-int inotifytools_get_stat_total( int event ) {
-	if (!collect_stats) return -1;
-	if ( IN_ACCESS == event )
-		return num_access;
-	if ( IN_MODIFY == event )
-		return num_modify;
-	if ( IN_ATTRIB == event )
-		return num_attrib;
-	if ( IN_CLOSE_WRITE == event )
-		return num_close_write;
-	if ( IN_CLOSE_NOWRITE == event )
-		return num_close_nowrite;
-	if ( IN_OPEN == event )
-		return num_open;
-	if ( IN_MOVED_FROM == event )
-		return num_moved_from;
-	if ( IN_MOVED_TO == event )
-		return num_moved_to;
-	if ( IN_CREATE == event )
-		return num_create;
-	if ( IN_DELETE == event )
-		return num_delete;
-	if ( IN_DELETE_SELF == event )
-		return num_delete_self;
-	if ( IN_UNMOUNT == event )
-		return num_unmount;
-	if ( IN_MOVE_SELF == event )
-		return num_move_self;
-
-	if ( 0 == event )
-		return num_total;
-
-	return -1;
-}
-
-/**
- * Get statistics by a particular filename.
- *
- * inotifytools_initialize_stats() must be called before this function can
- * be used.
- *
- * @param filename name of file to get stats for.
- *
- * @param event a single inotify event to get statistics for, or 0 for event
- *              total.  See section \ref events.
- *
- * @return the number of times the event specified by @a event has occurred on
- *         the file specified by @a filename since stats collection was
- *         enabled, or -1 if the file is not being watched or @a event is
- *         invalid.
- *
- * @note The filename specified must always be the original name used to
- *       establish the watch.
- */
-int inotifytools_get_stat_by_filename( char const * filename,
-                                                int event ) {
-	return inotifytools_get_stat_by_wd( inotifytools_wd_from_filename(
-	       filename ), event );
 }
 
 /**

libinotifytools/src/inotifytools_p.h
@@ -3,8 +3,49 @@
 
 #include "redblack.h"
 
-struct rbtree *inotifytools_wd_sorted_by_event(int sort_event);
+/**
+ * @internal
+ * Assert that a condition evaluates to true, and optionally output a message
+ * if the assertion fails.
+ *
+ * @param  cond  Integer; if 0, assertion fails, otherwise assertion succeeds.
+ *
+ * @param  mesg  A human-readable error message shown if assertion fails.
+ *
+ * @section example Example
+ * @code
+ * int upper = 100, lower = 50;
+ * int input = get_user_input();
+ * niceassert( input <= upper && input >= lower,
+ *             "input not in required range!");
+ * @endcode
+ */
+#define niceassert(cond,mesg) _niceassert((long)cond, __LINE__, __FILE__, \
+                                          #cond, mesg)
 
+
+										  /**
+* @internal
+* Assert that a condition evaluates to true, and optionally output a message
+* if the assertion fails.
+*
+* You should use the niceassert() preprocessor macro instead.
+*
+* @param  cond  If 0, assertion fails, otherwise assertion succeeds.
+*
+* @param  line  Line number of source code where assertion is made.
+*
+* @param  file  Name of source file where assertion is made.
+*
+* @param  condstr  Stringified assertion expression.
+*
+* @param  mesg  A human-readable error message shown if assertion fails.
+*/
+void _niceassert( long cond, int line, char const * file,
+                  char const * condstr, char const * mesg );
+
+struct rbtree *inotifytools_wd_sorted_by_event(int sort_event);
+extern int init;
 typedef struct watch {
 	char *filename;
 	int wd;
@@ -23,5 +64,5 @@ typedef struct watch {
 	unsigned hit_move_self;
 	unsigned hit_total;
 } watch;
-
+extern struct rbtree *tree_wd;
 #endif

libinotifytools/src/stats.c
@@ -1,0 +1,277 @@
+#include "stats.h"
+
+static unsigned  num_access;
+static unsigned  num_modify;
+static unsigned  num_attrib;
+static unsigned  num_close_nowrite;
+static unsigned  num_close_write;
+static unsigned  num_open;
+static unsigned  num_move_self;
+static unsigned  num_moved_to;
+static unsigned  num_moved_from;
+static unsigned  num_create;
+static unsigned  num_delete;
+static unsigned  num_delete_self;
+static unsigned  num_unmount;
+static unsigned  num_total;
+
+
+/**
+ * @internal
+ */
+void empty_stats(const void *nodep,
+                 const VISIT which,
+                 const int depth, void *arg) {
+    if (which != endorder && which != leaf) return;
+	watch *w = (watch*)nodep;
+	w->hit_access = 0;
+	w->hit_modify = 0;
+	w->hit_attrib = 0;
+	w->hit_close_nowrite = 0;
+	w->hit_close_write = 0;
+	w->hit_open = 0;
+	w->hit_move_self = 0;
+	w->hit_moved_from = 0;
+	w->hit_moved_to = 0;
+	w->hit_create = 0;
+	w->hit_delete = 0;
+	w->hit_delete_self = 0;
+	w->hit_unmount = 0;
+	w->hit_total = 0;
+}
+
+/**
+ * @internal
+ */
+void record_stats( struct inotify_event const * event ) {
+	if (!event) return;
+	watch *w = watch_from_wd(event->wd);
+	if (!w) return;
+	if ( IN_ACCESS & event->mask ) {
+		++w->hit_access;
+		++num_access;
+	}
+	if ( IN_MODIFY & event->mask ) {
+		++w->hit_modify;
+		++num_modify;
+	}
+	if ( IN_ATTRIB & event->mask ) {
+		++w->hit_attrib;
+		++num_attrib;
+	}
+	if ( IN_CLOSE_WRITE & event->mask ) {
+		++w->hit_close_write;
+		++num_close_write;
+	}
+	if ( IN_CLOSE_NOWRITE & event->mask ) {
+		++w->hit_close_nowrite;
+		++num_close_nowrite;
+	}
+	if ( IN_OPEN & event->mask ) {
+		++w->hit_open;
+		++num_open;
+	}
+	if ( IN_MOVED_FROM & event->mask ) {
+		++w->hit_moved_from;
+		++num_moved_from;
+	}
+	if ( IN_MOVED_TO & event->mask ) {
+		++w->hit_moved_to;
+		++num_moved_to;
+	}
+	if ( IN_CREATE & event->mask ) {
+		++w->hit_create;
+		++num_create;
+	}
+	if ( IN_DELETE & event->mask ) {
+		++w->hit_delete;
+		++num_delete;
+	}
+	if ( IN_DELETE_SELF & event->mask ) {
+		++w->hit_delete_self;
+		++num_delete_self;
+	}
+	if ( IN_UNMOUNT & event->mask ) {
+		++w->hit_unmount;
+		++num_unmount;
+	}
+	if ( IN_MOVE_SELF & event->mask ) {
+		++w->hit_move_self;
+		++num_move_self;
+	}
+
+	++w->hit_total;
+	++num_total;
+
+}
+
+unsigned int *stat_ptr(watch *w, int event)
+{
+	if ( IN_ACCESS == event )
+		return &w->hit_access;
+	if ( IN_MODIFY == event )
+		return &w->hit_modify;
+	if ( IN_ATTRIB == event )
+		return &w->hit_attrib;
+	if ( IN_CLOSE_WRITE == event )
+		return &w->hit_close_write;
+	if ( IN_CLOSE_NOWRITE == event )
+		return &w->hit_close_nowrite;
+	if ( IN_OPEN == event )
+		return &w->hit_open;
+	if ( IN_MOVED_FROM == event )
+		return &w->hit_moved_from;
+	if ( IN_MOVED_TO == event )
+		return &w->hit_moved_to;
+	if ( IN_CREATE == event )
+		return &w->hit_create;
+	if ( IN_DELETE == event )
+		return &w->hit_delete;
+	if ( IN_DELETE_SELF == event )
+		return &w->hit_delete_self;
+	if ( IN_UNMOUNT == event )
+		return &w->hit_unmount;
+	if ( IN_MOVE_SELF == event )
+		return &w->hit_move_self;
+	if ( 0 == event )
+		return &w->hit_total;
+	return 0;
+}
+
+/**
+ * Get statistics by a particular watch descriptor.
+ *
+ * inotifytools_initialize_stats() must be called before this function can
+ * be used.
+ *
+ * @param wd watch descriptor to get stats for.
+ *
+ * @param event a single inotify event to get statistics for, or 0 for event
+ *              total.  See section \ref events.
+ *
+ * @return the number of times the event specified by @a event has occurred on
+ *         the watch descriptor specified by @a wd since stats collection was
+ *         enabled, or -1 if @a event or @a wd are invalid.
+ */
+int inotifytools_get_stat_by_wd( int wd, int event ) {
+	if (!collect_stats) return -1;
+
+	watch *w = watch_from_wd(wd);
+	if (!w) return -1;
+	unsigned int *i = stat_ptr(w, event);
+	if (!i) return -1;
+	return *i;
+}
+
+/**
+ * Get statistics aggregated across all watches.
+ *
+ * inotifytools_initialize_stats() must be called before this function can
+ * be used.
+ *
+ * @param event a single inotify event to get statistics for, or 0 for event
+ *              total.  See section \ref events.
+ *
+ * @return the number of times the event specified by @a event has occurred over
+ *         all watches since stats collection was enabled, or -1 if @a event
+ *         is not a valid event.
+ */
+int inotifytools_get_stat_total( int event ) {
+	if (!collect_stats) return -1;
+	if ( IN_ACCESS == event )
+		return num_access;
+	if ( IN_MODIFY == event )
+		return num_modify;
+	if ( IN_ATTRIB == event )
+		return num_attrib;
+	if ( IN_CLOSE_WRITE == event )
+		return num_close_write;
+	if ( IN_CLOSE_NOWRITE == event )
+		return num_close_nowrite;
+	if ( IN_OPEN == event )
+		return num_open;
+	if ( IN_MOVED_FROM == event )
+		return num_moved_from;
+	if ( IN_MOVED_TO == event )
+		return num_moved_to;
+	if ( IN_CREATE == event )
+		return num_create;
+	if ( IN_DELETE == event )
+		return num_delete;
+	if ( IN_DELETE_SELF == event )
+		return num_delete_self;
+	if ( IN_UNMOUNT == event )
+		return num_unmount;
+	if ( IN_MOVE_SELF == event )
+		return num_move_self;
+
+	if ( 0 == event )
+		return num_total;
+
+	return -1;
+}
+
+/**
+ * Get statistics by a particular filename.
+ *
+ * inotifytools_initialize_stats() must be called before this function can
+ * be used.
+ *
+ * @param filename name of file to get stats for.
+ *
+ * @param event a single inotify event to get statistics for, or 0 for event
+ *              total.  See section \ref events.
+ *
+ * @return the number of times the event specified by @a event has occurred on
+ *         the file specified by @a filename since stats collection was
+ *         enabled, or -1 if the file is not being watched or @a event is
+ *         invalid.
+ *
+ * @note The filename specified must always be the original name used to
+ *       establish the watch.
+ */
+int inotifytools_get_stat_by_filename( char const * filename,
+                                                int event ) {
+	return inotifytools_get_stat_by_wd( inotifytools_wd_from_filename(
+	       filename ), event );
+}
+
+
+/**
+ * Initialize or reset statistics.
+ *
+ * inotifytools_initialize() must be called before this function can
+ * be used.
+ *
+ * When this function is called, all subsequent events will be tallied.
+ * Statistics can then be obtained via the @a inotifytools_get_stat_* functions.
+ *
+ * After the first call, subsequent calls to this function will reset the
+ * event tallies to 0.
+ */
+void inotifytools_initialize_stats() {
+	niceassert( init, "inotifytools_initialize not called yet" );
+
+	// if already collecting stats, reset stats
+	if (collect_stats) {
+		rbwalk(tree_wd, empty_stats, 0);
+	}
+
+	num_access = 0;
+	num_modify = 0;
+	num_attrib = 0;
+	num_close_nowrite = 0;
+	num_close_write = 0;
+	num_open = 0;
+	num_move_self = 0;
+	num_moved_from = 0;
+	num_moved_to = 0;
+	num_create = 0;
+	num_delete = 0;
+	num_delete_self = 0;
+	num_unmount = 0;
+	num_total = 0;
+
+	collect_stats = 1;
+}
+

libinotifytools/src/Makefile.am
@@ -1,7 +1,7 @@
 SUBDIRS = inotifytools
 
 lib_LTLIBRARIES = libinotifytools.la
-libinotifytools_la_SOURCES = inotifytools.c inotifytools_p.h redblack.c redblack.h
+libinotifytools_la_SOURCES = inotifytools.c inotifytools_p.h redblack.c redblack.h stats.c stats.h
 libinotifytools_la_LDFLAGS = -version-info 4:1:4
 
 check_PROGRAMS = test

libinotifytools/src/stats.h
@@ -1,0 +1,12 @@
+#ifndef STATS_H
+#define STATS_H
+#include "inotifytools.h"
+#include "inotifytools/inotify.h"
+#include "inotifytools_p.h"
+
+
+extern int collect_stats;
+void record_stats( struct inotify_event const * event );
+unsigned int *stat_ptr(watch *w, int event);
+watch *watch_from_wd(int wd);
+#endif // STATS_H

commit c982e0ef4950bf03c6b5055e854222b800df2c05

libinotifytools: Rename init variable to fix conflict with entry point
Running inotifywait 3.22.1.0 on Ubuntu 22.04, I observed a bug
apparently due to a bad file descriptor:

    $ inotifywait -e modify foo
    Setting up watches.
    Couldn't watch foo: Bad file descriptor

I could also reproduce with the master branch. Investigating further, it
appears that inotifytools_init() in inotifytools.c exits immediately
after entry, because the global variable "init" is non-null, even though
we just called it at the beginning of inotifywait.c's main(), and the
global variable is supposed to be initialised at 0.

Bisecting showed that this variable used to be static, but was changed
at some point to non-static to share it with stats.c, and this is where
the bug fist occurs. As far as I understand, the symbol conflicts with
the init() function from the program's entry point [0]. When entering
inotifytools_init(), it seems that the value for that function is picked
up and compared to 0, found non-null, and the initialization function
exits, leaving inotify_fd uninitialized (-1) and a bad file descriptor.

Renaming the variable to something other than "init" is enough to fix
the bug.

    $ ./src/inotifywait -e modify foo
    Setting up watches.
    Watches established
    <waits>

[0] https://www.geeksforgeeks.org/executing-main-in-c-behind-the-scene/

Fixes: dd59c5347af8 ("Extract stats into their own file (#129)")

libinotifytools/src/inotifytools.c
@@ -157,7 +157,7 @@ struct rbtree *tree_wd = 0;
 struct rbtree* tree_fid = 0;
 struct rbtree *tree_filename = 0;
 static int error = 0;
-int init = 0;
+int initialized = 0;
 int verbosity = 0;
 int fanotify_mode = 0;
 int fanotify_mark_type = 0;
@@ -299,7 +299,8 @@ watch *watch_from_filename( char const *filename ) {
  *         obtained from inotifytools_error().
  */
 int inotifytools_init(int fanotify, int watch_filesystem, int verbose) {
-	if (init) return 1;
+	if (initialized)
+		return 1;
 
 	error = 0;
 	verbosity = verbose;
@@ -323,7 +324,7 @@ int inotifytools_init(int fanotify, int watch_filesystem, int verbose) {
 	}
 
 	collect_stats = 0;
-	init = 1;
+	initialized = 1;
 	tree_wd = rbinit(wd_compare, 0);
 	tree_fid = rbinit(fid_compare, 0);
 	tree_filename = rbinit(filename_compare, 0);
@@ -366,9 +367,10 @@ void cleanup_tree(const void *nodep,
  * again before any other functions can be used.
  */
 void inotifytools_cleanup() {
-	if (!init) return;
+	if (!initialized)
+		return;
 
-	init = 0;
+	initialized = 0;
 	close(inotify_fd);
 	collect_stats = 0;
 	error = 0;
@@ -875,7 +877,7 @@ const char* inotifytools_filename_from_watch(watch* w) {
  *       filename returned will still be the original name.
  */
 const char* inotifytools_filename_from_wd(int wd) {
-	niceassert( init, "inotifytools_initialize not called yet" );
+	niceassert(initialized, "inotifytools_initialize not called yet");
 	if (!wd)
 		return "";
 	watch *w = watch_from_wd(wd);
@@ -980,7 +982,7 @@ char* inotifytools_dirpath_from_event(struct inotify_event* event) {
  *       establish the watch.
  */
 int inotifytools_wd_from_filename( char const * filename ) {
-	niceassert( init, "inotifytools_initialize not called yet" );
+	niceassert(initialized, "inotifytools_initialize not called yet");
 	if (!filename || !*filename)
 		return -1;
 	watch *w = watch_from_filename(filename);
@@ -1003,7 +1005,7 @@ int inotifytools_wd_from_filename( char const * filename ) {
  * @param filename New filename.
  */
 void inotifytools_set_filename_by_wd( int wd, char const * filename ) {
-	niceassert( init, "inotifytools_initialize not called yet" );
+	niceassert(initialized, "inotifytools_initialize not called yet");
 	watch *w = watch_from_wd(wd);
 	if (!w) return;
 	if (w->filename) free(w->filename);
@@ -1125,7 +1127,7 @@ watch* create_watch(int wd,
  *         obtained from inotifytools_error().
  */
 int inotifytools_remove_watch_by_wd( int wd ) {
-	niceassert( init, "inotifytools_initialize not called yet" );
+	niceassert(initialized, "inotifytools_initialize not called yet");
 	watch *w = watch_from_wd(wd);
 	if (!w) return 1;
 
@@ -1150,7 +1152,7 @@ int inotifytools_remove_watch_by_wd( int wd ) {
  *       establish the watch.
  */
 int inotifytools_remove_watch_by_filename( char const * filename ) {
-	niceassert( init, "inotifytools_initialize not called yet" );
+	niceassert(initialized, "inotifytools_initialize not called yet");
 	watch *w = watch_from_filename(filename);
 	if (!w) return 1;
 
@@ -1197,7 +1199,7 @@ int inotifytools_watch_file(char const* filename, int events) {
  *         obtained from inotifytools_error().
  */
 int inotifytools_watch_files(char const* filenames[], int events) {
-	niceassert( init, "inotifytools_initialize not called yet" );
+	niceassert(initialized, "inotifytools_initialize not called yet");
 	error = 0;
 
 	static int i;
@@ -1423,7 +1425,7 @@ struct inotify_event * inotifytools_next_event( long int timeout ) {
  *       the @a timeout period begins again each time a matching event occurs.
  */
 struct inotify_event * inotifytools_next_events( long int timeout, int num_events ) {
-	niceassert( init, "inotifytools_initialize not called yet" );
+	niceassert(initialized, "inotifytools_initialize not called yet");
 	niceassert( num_events <= MAX_EVENTS, "too many events requested" );
 
 	if ( num_events < 1 ) return NULL;
@@ -1718,7 +1720,7 @@ int inotifytools_watch_recursively(char const* path, int events) {
 int inotifytools_watch_recursively_with_exclude(char const* path,
 						int events,
 						char const** exclude_list) {
-	niceassert( init, "inotifytools_initialize not called yet" );
+	niceassert(initialized, "inotifytools_initialize not called yet");
 
 	DIR * dir;
 	char * my_path;

libinotifytools/src/inotifytools_p.h
@@ -45,7 +45,7 @@ void _niceassert( long cond, int line, char const * file,
                   char const * condstr, char const * mesg );
 
 struct rbtree *inotifytools_wd_sorted_by_event(int sort_event);
-extern int init;
+extern int initialized;
 
 struct fanotify_event_fid;
 

libinotifytools/src/stats.c
@@ -250,7 +250,7 @@ int inotifytools_get_stat_by_filename( char const * filename,
  * event tallies to 0.
  */
 void inotifytools_initialize_stats() {
-	niceassert( init, "inotifytools_initialize not called yet" );
+	niceassert(initialized, "inotifytools_initialize not called yet");
 
 	// if already collecting stats, reset stats
 	if (collect_stats) {