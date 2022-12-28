f475874f82c19fbc0db02f966626c1afeacfd601
Eric Curtin <ecurtin@redhat.com>
2022-12-28 15:34:31 +0000

    Alpine

---
+++ build_and_test_multi.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+if command -v podman > /dev/null; then
+  container="podman"
+elif command -v docker > /dev/null; then
+  container="docker"
+fi
+
+container_run() {
+  cmd="./build_and_test.sh"
+  id=$(sudo $container run --privileged -d -it $1 /bin/bash)
+  sudo $container exec -it $id /bin/sh -c "mkdir -p $PWD"
+  sudo $container cp $PWD $id:$PWD/..
+  sudo $container exec -it $id /bin/sh -c "cd $PWD && $cmd"
+  sudo $container rm -f $id
+}
+
+if [ -n "$container" ]; then
+  container_run "alpine:3.17"
+  container_run "centos:stream9"
+  container_run "fedora:37"
+  container_run "ubuntu:22.04"
+  exit 0
+fi
+
+$cmd
+
